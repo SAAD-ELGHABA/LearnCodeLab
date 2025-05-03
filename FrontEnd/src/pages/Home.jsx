c63eb39647b22a160ca0c3f02530d15121cbcd43
SaadBaouli <baoulisaad@gmail.com>
2025-05-03 17:45:59 +0100

    Pages : About , Blog , Block , Contact-Us ...

--- FrontEnd/src/pages/Home.jsx
+++ FrontEnd/src/pages/Home.jsx
@@ -96,50 +96,6 @@ export default function HomePage() {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
-<<<<<<< HEAD
-=======
-  return (
-    <>
-      <div className=" text-white min-h-screen z-50">
-        <div className="flex items-end container mx-auto justify-evenly">
-          <header className="text-start pt-20 w-2/5">
-            <h1 className="text-4xl md:text-5xl font-bold text-blue-400 drop-shadow-lg uppercase">
-              Your Way to Get <br /> Your Diploma!
-            </h1>
-            <p className="mt-4 text-gray-300 w-2/3">
-              This is LearnCodeLab, your assistant in the world of programming.
-              Learn, Practice, and Share Your Code with Ease.
-            </p>
-            <div className="flex space-x-2">
-              <button className="mt-6 px-6 py-3 text-blue-400 border cursor-pointer border-blue-400 rounded-md transition w-1/4 text-sm">
-                Let&apos;s start!
-              </button>
-              <button className="bg-blue-400 mt-6 px-6 py-3 border hover:text-white cursor-pointer border-blue-400 hover:bg-blue-600 rounded-md transition w-1/4 text-sm">
-                sign in
-              </button>
-            </div>
-          </header>
-          <div className="flex justify-center mt-10 w-1/2">
-            <img
-              src={peapleCoding}
-              alt="People coding"
-              className="w-full drop-shadow-xl"
-            />
-          </div>
-        </div>
-        <motion.div
-          id="animated-section"
-          initial={{ opacity: 0, y: 50 }}
-          animate={isVisible ? { opacity: 1, y: 0 } : {}}
-          transition={{ duration: 0.8, ease: "easeOut" }}
-          className=" text-white rounded-lg text-center mt-20"
-        >
-          <section className="mt-16 px-6">
-            <fieldset className="border-1 border-[#93c4fd60] p-6 rounded">
-              <legend className="text-3xl font-bold text-center px-4">
-                Why Learn Code Lab?
-              </legend>
->>>>>>> 6c3e706ec19a058a0c5efb737650954ac2a1c8af
 
   return (
     <div className="text-white min-h-screen z-50 bg-[#0f172a]">
