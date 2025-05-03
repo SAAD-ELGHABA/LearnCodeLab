5e8c064ec76e42c1af6a7f9f9d898c033d2cb28e
Douissy Mohcine <mdwissy@gmail.com>
2025-05-03 18:51:40 +0100

    Add responsive

--- FrontEnd/src/pages/Home.jsx
+++ FrontEnd/src/pages/Home.jsx
@@ -21,25 +21,25 @@ export default function HomePage() {
   return (
     <>
       <div className=" text-white min-h-screen z-50">
-        <div className="flex items-end container mx-auto justify-evenly">
-          <header className="text-start pt-20 w-2/5">
-            <h1 className="text-4xl md:text-5xl font-bold text-blue-400 drop-shadow-lg uppercase">
-              Your Way to Get <br /> Your Diploma!
+        <div className="lg:flex items-end container mx-auto justify-evenly">
+          <header className="text-start pt-20 lg:w-2/5  ">
+            <h1 className="text-4xl md:text-5xl font-bold text-blue-400 drop-shadow-lg uppercase text-center sm:text-left">
+              The Way to Get <br /> Your Diploma!
             </h1>
-            <p className="mt-4 text-gray-300 w-2/3">
-              This is LearnCodeLab, your assistant in the world of programming.
+            <p className="mt-4 text-gray-300 lg:w-2/3 w-4/5 sm:w-full text-center sm:text-left mx-auto sm:mx-0">
+              This is br LearnCodeLab, your assistant in the world of programming.
               Learn, Practice, and Share Your Code with Ease.
             </p>
-            <div className="flex space-x-2">
-              <button className="mt-6 px-6 py-3 text-blue-400 border cursor-pointer border-blue-400 rounded-md transition w-1/4 text-sm">
+            <div className="flex flex-col sm:flex-row justify-center sm:justify-start items-center sm:items-start  sm:space-y-0 sm:space-x-2 mt-6">
+            <button className="mt-6 px-6 py-3 text-blue-400 border cursor-pointer border-blue-400 rounded-md transition text-sm w-1/2 sm:w-1/2 md:w-1/4">
                 Let&apos;s start!
               </button>
-              <button className="bg-blue-400 mt-6 px-6 py-3 border hover:text-white cursor-pointer border-blue-400 hover:bg-blue-600 rounded-md transition w-1/4 text-sm">
+              <button className="bg-blue-400 mt-6 px-6 py-3 border hover:text-white cursor-pointer border-blue-400 hover:bg-blue-600 rounded-md transition text-sm w-1/2 sm:w-1/2 md:w-1/4">
                 sign in
               </button>
             </div>
           </header>
-          <div className="flex justify-center mt-10 w-1/2">
+          <div className="flex justify-center mt-10 md:w-1/2 ">
             <img
               src={peapleCoding}
               alt="People coding"
